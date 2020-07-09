695050ea04703ab21d399902a1af7c13445b05c7
Yosheng <yosheng0323@outlook.com>
2020-07-09 18:07:01 +0800

    添加项目文件。

---
+++ TwnSum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TwnSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = TwoSum(new int[] {2, 7, 11, 15}, 9);
+            Console.WriteLine("[{0}]", string.Join(", ", result));
+        }
+
+        private static int[] TwoSum(int[] nums, int target) {
+            int[] twoSum = new int[2];
+            for (int i= 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        twoSum[0] = i;
+                        twoSum[1] = j;
+                    }
+                }
+            }
+            return twoSum;
+        }
+    }
+}
