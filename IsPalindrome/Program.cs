4355ddc2b80954e8aef06a32d5377d06fe50adf6
Yosheng <yosheng0323@outlook.com>
2020-07-14 13:32:42 +0800

    添加回文数

---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace IsPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindromeByString(121));
+            Console.WriteLine(IsPalindromeByString(-121));
+            Console.WriteLine(IsPalindromeByString(10));
+            
+            Console.WriteLine(IsPalindromeByNum(121));
+            Console.WriteLine(IsPalindromeByNum(-121));
+            Console.WriteLine(IsPalindromeByNum(10));
+        }
+        
+        private static bool IsPalindromeByString(int x)
+        {
+            var str = x.ToString();
+            var reverseStr = string.Empty;
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverseStr = reverseStr + str[i];
+            }
+            
+            return str == reverseStr;
+        }
+
+        private static bool IsPalindromeByNum(int x)
+        {
+            if (x < 0 || (x >= 10 && x % 10 == 0))
+            {
+                return false;
+            }
+            int revert = 0;
+            while (x > revert)
+            {
+                revert = revert * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revert || x == (revert / 10);
+        }
+    }
+}
--- Reverse/Program.cs
+++ Reverse/Program.cs
@@ -6,7 +6,40 @@ namespace Reverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 输出: 321
+            Console.WriteLine(Reverse(123));
+            
+            // 输出: -321
+            Console.WriteLine(Reverse(-123));
+            
+            // 输出: 21
+            Console.WriteLine(Reverse(120));
+        }
+
+        private static int Reverse(int num)
+        {
+            var numStr = num < 0 ? (num*-1).ToString() : num.ToString();
+
+            var reverseStr = string.Empty;
+
+            for (int i = numStr.Length - 1; i >= 0; i--)
+            {
+                reverseStr += numStr[i];
+            }
+
+            if (num < 0)
+            {
+                reverseStr = $"-{reverseStr}";
+            }
+
+            try
+            {
+                return int.Parse(reverseStr);
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
         }
     }
 }
